f9305823446109b4cdffef9f84583cfbe95f471a
Martin Ballasco Ramos <martinballasco@gmail.com>
2019-02-17 11:32:00 -0500

    Adding the whole project

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public class StudyClass {
+                public String className { get; set; }
+                public List<Student> Students { get; set; }
+        }
+
+        public class Student {
+                public String studentName { get; set; }
+        }
+
+        private static StudyClass GetClassData()
+        {
+            StudyClass Sc = new StudyClass();
+            return Sc;
+            
+        }
+
+        static void Main(string[] args)
+        {
+
+            var classObject = GetClassData();
+
+            var StudyClass = new StudyClass()
+            {
+                className = classObject.Students
+            };
+            foreach (var student in classObject.students)
+            {
+                    StudyClass.
+            }
+         }
+
+        
+    }
+}
---
+++ Sistema.Datos/DbContextSistema.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sistema.Datos.Mapping.Almacen;
+using Sistema.Entidades.Almacen;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sistema.Datos
+{
+    public class DbContextSistema : DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }   //Exponiendo la coleccion categorias en el un objeto categorias
+         
+        public DbContextSistema(DbContextOptions<DbContextSistema> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfiguration(new CategoriaMap());
+        }
+    }
+}
+
+
+
