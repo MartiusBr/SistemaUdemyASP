2daeb6712a9acdb86082cd367864419b814b0930
Martin Ballasco Ramos <martinballasco@gmail.com>
2019-02-19 21:13:23 -0500

    Adding the Artículos Controller and ArtículosViewModel

---
+++ Sistema.Web/Controllers/ArticulosController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Sistema.Datos;
+using Sistema.Entidades.Almacen;
+using Sistema.Web.Models.Almacen.Articulo;
+
+namespace Sistema.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArticulosController : ControllerBase
+    {
+        private readonly DbContextSistema _context;
+
+        public ArticulosController(DbContextSistema context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Articulos/Listar
+        [HttpGet("[action]")]
+        public async Task<IEnumerable<ArticuloViewModel>> Listar()
+        {
+            var articulo = await _context.Articulos.Include(a => a.categoria).ToListAsync();
+
+            return articulo.Select(a => new ArticuloViewModel
+            {
+                idarticulo = a.idarticulo,
+                idcategoria = a.idcategoria,
+                categoria = a.categoria.nombre,
+                codigo = a.codigo,
+                nombre = a.nombre,
+                stock = a.stock,
+                precio_venta = a.precio_venta,
+                descripcion = a.descripcion,
+                condicion = a.condicion
+            });
+
+        }
+
+        // GET: api/Articulos/Mostrar/1
+        [HttpGet("[action]/{id}")]
+        public async Task<IActionResult> Mostrar([FromRoute] int id)
+        {
+
+            var articulo = await _context.Articulos.Include(a =>a.categoria).
+                SingleOrDefaultAsync(a => a.idarticulo==id);
+
+            if (articulo == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(new ArticuloViewModel
+            {
+                idarticulo = articulo.idarticulo,
+                idcategoria = articulo.idcategoria,
+                categoria = articulo.categoria.nombre,
+                codigo = articulo.codigo,
+                nombre = articulo.nombre,
+                descripcion = articulo.descripcion,
+                stock = articulo.stock,
+                precio_venta = articulo.precio_venta,
+                condicion = articulo.condicion
+            });
+        }
+
+        private bool ArticuloExists(int id)
+        {
+            return _context.Articulos.Any(e => e.idarticulo == id);
+        }
+    }
+}
